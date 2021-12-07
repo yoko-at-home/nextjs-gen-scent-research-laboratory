927861c02cbda0cb9fbfa390c36172de6b53f63d
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-12-08 08:58:47 +0900

    subtitleのスタイルを追加

--- src/component/PageTitle.js
+++ src/component/PageTitle.js
@@ -1,7 +1,10 @@
 export const PageTitle = (props) => {
   return (
-    <h1 className="mt-20 mb-5 text-3xl font-extrabold tracking-tight leading-9 sm:text-4xl sm:leading-10 md:mt-32 md:mb-16 md:text-5xl md:leading-10 lg:mb-10">
+    <h1 className="mt-20 mb-5 text-3xl font-extrabold tracking-tight leading-9 sm:text-4xl sm:leading-10 md:mt-24 md:mb-16 md:text-5xl md:leading-10 lg:mb-10">
       {props.children}
     </h1>
   );
+};
+export const PageSubTitle = (props) => {
+  return <h2 className="mt-10 mb-5 text-2xl font-bold md:mt-14 md:mb-8 md:text-3xl">{props.children}</h2>;
 };
