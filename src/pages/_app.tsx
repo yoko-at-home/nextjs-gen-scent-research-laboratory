0986de4957a504438482d248f94589e53e66e0d5
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-12-07 17:03:24 +0900

    _app/_documentの追加

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import "/src/style/index.css";
+
+import type { AppProps } from "next/app";
+
+const App = (props: AppProps) => {
+  return <props.Component {...props.pageProps} />;
+};
+
+export default App;
