733085b288059ce7dab9ecf3163382ac9151793d
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-12-07 13:00:12 +0900

    Footerを更新

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,4 +1,6 @@
+import Link from "next/link";
 import type { VFC } from "react";
+import { siteMetadata } from "src/data/siteMetaData";
 
 /**
  * @package
@@ -6,7 +8,21 @@ import type { VFC } from "react";
 export const Footer: VFC = () => {
   return (
     <footer>
-      <small>&copy; 20xx example</small>
+      <div className="flex flex-col items-center mt-32">
+        <div className="flex mb-2 space-x-2 text-sm">
+          <div>{siteMetadata.author}</div>
+          <div>{`© ${new Date().getFullYear()}`}</div>
+          <div>All Rights Reserved.</div>
+        </div>
+        <div className="my-8 text-sm text-gray-400">
+          <Link href="https://over40webclub.netlify.app/">
+            <a target="_blank" rel="noopener noreferrer">
+              Powered by Over 40 Web Club
+            </a>
+          </Link>
+        </div>
+        {/* <NavBarMobile /> */}
+      </div>
     </footer>
   );
 };
