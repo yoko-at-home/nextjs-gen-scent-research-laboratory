223e8cc178693c197ad41be849a4b71ca0fe94db
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-12-07 21:03:11 +0900

    software一覧をmicrocmsから取得

--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -1,6 +1,33 @@
 /* eslint-disable import/no-default-export */
-const Product = () => {
-  return <div>Productページです。</div>;
-};
+import Link from "next/link";
 
-export default Product;
+import { client } from "../../lib/client";
+
+export default function Product({ software }) {
+  return (
+    <div>
+      <ul>
+        {software.map((software) => {
+          return (
+            <li key={software.id}>
+              <Link href={`/software/${software.id}`}>
+                <a>{software.title}</a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async () => {
+  const data = await client.get({ endpoint: "software" });
+
+  return {
+    props: {
+      software: data.contents,
+    },
+  };
+};
