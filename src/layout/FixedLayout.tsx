ecbb3e8037b22c95ced7b26a92c0024e1615488e
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-12-07 17:10:21 +0900

    childrenを表示するように変更

--- src/layout/FixedLayout.tsx
+++ src/layout/FixedLayout.tsx
@@ -1,18 +1,24 @@
-import type { CustomLayout } from "next";
+import type { ReactNode, VFC } from "react";
 
 import { Footer } from "./Footer";
 import { Header } from "./Header";
 import { LayoutErrorBoundary } from "./LayoutErrorBoundary";
 
+type Props = {
+  className?: string;
+  theme?: "main" | "about" | "newsArticles";
+  children: ReactNode;
+};
+
 /**
  * @package
  */
-export const FixedLayout: CustomLayout = (page) => {
+export const FixedLayout: VFC<Props> = (props) => {
   return (
     <div className="grid grid-rows-[auto,1fr,auto] mx-auto max-w-screen-md min-h-screen">
       <Header />
-      <main className="bg-blue-300">
-        <LayoutErrorBoundary>{page}</LayoutErrorBoundary>
+      <main className="mx-5 sm:mx-10 md:mx-auto md:w-11/12 lg:w-9/12">
+        <LayoutErrorBoundary>{props.children}</LayoutErrorBoundary>
       </main>
       <Footer />
     </div>
