ad5c761de536c2a28ee991a3fe12f9910597d095
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-12-07 12:59:48 +0900

    metadataの追加

---
+++ src/data/siteMetaData.js
@@ -0,0 +1,15 @@
+export const siteMetadata = {
+  author: "Gen-Scent Research Laboratory",
+  title: "Gen-Scent Research Laboratory",
+  headerTitle: "Gen-Scent Research Laboratory",
+  description:
+    "玄川リサーチは、におい分析を行う研究者の皆様のお役に立つべく、2021年に開業したばかりのラボラトリです。予算が厳しい中でも何とか良い研究成果を出したいという方にとって有用な分析ツールの販売や、分析手法やデータ解析で悩んでおられる方と一緒に良い結果を導くお手伝いを行います。",
+  language: "jp-ja",
+  siteUrl: "https://www.gen-scent.com",
+  siteRepo: "https://www.gen-scent.com",
+  siteLogo: "/static/images/logo.png",
+  image: "/static/images/laurel.png",
+  socialBanner: "/static/images/logo.png",
+  email: "yoko_iwasakijp@yahoo.co.jp",
+  locale: "ja-JP",
+};
