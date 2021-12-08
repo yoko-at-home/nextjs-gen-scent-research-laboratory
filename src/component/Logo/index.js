06770bc59cd90cb9ba8273afd245bf33dc249ed3
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-12-08 09:01:04 +0900

    ロゴをコンポーネント化

---
+++ src/component/Logo/index.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Logo = () => {
+  return (
+    <>
+      <div className="hidden absolute top-7 right-20 z-30 lg:block">
+        <Link href="/" aria-label="Gen-Scent Research Laboratory" passHref>
+          <Image src={"/static/images/logo.png"} alt="logo" width="70px" height="70px" />
+        </Link>
+      </div>
+      <div className="absolute top-5 right-16 z-30 lg:hidden">
+        <Link href="/" aria-label="Gen-Scent Research Laboratory" passHref>
+          <Image src={"/static/images/logo.png"} alt="logo" width="50px" height="50px" />
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default Logo;
--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
-import Link from "next/link";
 import type { VFC } from "react";
 import { NavLink } from "src/component/Button";
+import Logo from "src/component/Logo";
 import { siteMetadata } from "src/data/siteMetaData";
 
 const items = [
@@ -21,11 +21,7 @@ export const Header: VFC = () => {
   return (
     <header>
       <div className="relative">
-        <div className="absolute top-5 right-20 z-30">
-          <Link href="/" aria-label="Gen-Scent Research Laboratory">
-            <Image src={"/static/images/logo.png"} alt="logo" width="50px" height="50px" />
-          </Link>
-        </div>
+        <Logo />
         <div className="absolute top-24 left-32 z-30 md:top-28">
           <h1 className="z-20 text-3xl text-white sm:text-5xl lg:text-6xl xl:text-7xl">{siteMetadata.author}</h1>
         </div>
@@ -48,7 +44,7 @@ export const Header: VFC = () => {
           />
         </div>
       </div>
-      <nav className="flex justify-center bg-gray-200">
+      <nav className="flex justify-center text-gray-600 bg-gray-200">
         {items.map(({ href, label }) => {
           return (
             <NavLink key={href} href={href} activeClassName="bg-gray-500 text-white">
