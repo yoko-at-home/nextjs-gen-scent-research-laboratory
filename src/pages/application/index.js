db197a695093538ebc316b9088324f8950ddb7be
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-12-09 16:23:25 +0900

    applicationページcmsからコンテンツを取得

--- src/pages/application/index.js
+++ src/pages/application/index.js
@@ -2,26 +2,46 @@ import { PageTitle } from "src/component/PageTitle";
 import { PageSEO } from "src/component/SEO";
 import { siteMetadata } from "src/data/siteMetaData";
 import { FluidLayout } from "src/layout";
+import { client } from "src/lib/client";
 
 /* eslint-disable import/no-default-export */
-const Application = () => {
+const Application = (props) => {
   return (
     <div
       style={{
         background: "center/cover no-repeat url('/static/images/coming-soon.jpg')",
       }}
-      className="z-10 text-gray-200"
+      className="z-10"
     >
-      <div className="z-40 bg-white bg-opacity-30">
+      <div className="z-40 bg-white bg-opacity-70">
         <FluidLayout>
           <PageSEO title={`Application - ${siteMetadata.author}`} description={siteMetadata.description} />
-          <PageTitle>
-            <div className="z-40">Coming soon….</div>
-          </PageTitle>
+          <div className="">
+            <PageTitle>{props.data.title}</PageTitle>
+            <div
+              className="py-5 px-5 text-lg md:py-10 lg:py-16 animation"
+              dangerouslySetInnerHTML={{
+                // eslint-disable-next-line @typescript-eslint/naming-convention
+                __html: `${props.data.body}`,
+              }}
+            />
+          </div>
         </FluidLayout>
       </div>
     </div>
   );
+};
+
+export const getStaticProps = async () => {
+  const data = await client.get({
+    endpoint: "application",
+  });
+
+  return {
+    props: {
+      data,
+    },
+  };
 };
 
 export default Application;
