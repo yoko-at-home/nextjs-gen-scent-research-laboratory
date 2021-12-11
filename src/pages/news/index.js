098a552175aab2a737134429dfa05838019f22f9
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-12-11 21:14:37 +0900

    コードを一旦削除

--- src/pages/news/index.js
+++ src/pages/news/index.js
@@ -1,31 +1,16 @@
-import { PageSubTitle, PageTitle } from "src/component/PageTitle";
+import { PageTitle } from "src/component/PageTitle";
 import { PageSEO } from "src/component/SEO";
 import { siteMetadata } from "src/data/siteMetaData";
 import { FluidLayout } from "src/layout";
 import { client } from "src/lib/client";
 
-const News = (props) => {
+const News = () => {
   return (
     <FluidLayout>
       <PageSEO title={`News - ${siteMetadata.author}`} description={siteMetadata.description} />
 
       <div className="">
         <PageTitle>最新情報</PageTitle>
-        <div
-          className="px-5 mb-20 text-gray-500 sm:text-lg"
-          dangerouslySetInnerHTML={{
-            // eslint-disable-next-line @typescript-eslint/naming-convention
-            __html: `${props.data.body}`,
-          }}
-        />
-        <PageSubTitle>2021年の活動</PageSubTitle>
-        <div
-          className="px-5 text-gray-500 sm:text-lg"
-          dangerouslySetInnerHTML={{
-            // eslint-disable-next-line @typescript-eslint/naming-convention
-            __html: `${props.data.body2021}`,
-          }}
-        />
       </div>
     </FluidLayout>
   );
