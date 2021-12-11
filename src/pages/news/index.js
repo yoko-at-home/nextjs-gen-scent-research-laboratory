70a9007463f4143a2c827ee396b879832dc98a5a
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-12-11 22:18:37 +0900

    newsにlistを対応

--- src/pages/news/index.js
+++ src/pages/news/index.js
@@ -1,10 +1,11 @@
+import Link from "next/link";
 import { PageTitle } from "src/component/PageTitle";
 import { PageSEO } from "src/component/SEO";
 import { siteMetadata } from "src/data/siteMetaData";
 import { FluidLayout } from "src/layout";
 import { client } from "src/lib/client";
 
-const News = () => {
+export default function News({ news }) {
   return (
     <FluidLayout>
       <PageSEO title={`News - ${siteMetadata.author}`} description={siteMetadata.description} />
@@ -12,9 +13,33 @@ const News = () => {
       <div className="">
         <PageTitle>最新情報</PageTitle>
       </div>
+      <ul>
+        {news.map((item) => {
+          return (
+            <li key={item.id}>
+              <div
+                className="flex flex-col p-8 mb-10 bg-gray-200 bg-opacity-50 rounded sm:p-3"
+                // style={{ background: `center/cover no-repeat url(${item.image.url})` }}
+              >
+                <div className="mb-3 text-xl font-semibold sm:font-bold">{item.title}</div>
+                <div className="flex flex-row-reverse justify-between items-end">
+                  <Link href={`/news/${item.id}`} passHref>
+                    <a>
+                      {item.detail === true ? (
+                        <span className="p-2 ml-5 text-gray-700 whitespace-nowrap bg-gray-300">詳細</span>
+                      ) : null}
+                    </a>
+                  </Link>
+                  <div className="overflow-scroll text-sm sm:text-base">{item.description}</div>
+                </div>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </FluidLayout>
   );
-};
+}
 
 export const getStaticProps = async () => {
   const data = await client.get({
@@ -23,10 +48,7 @@ export const getStaticProps = async () => {
 
   return {
     props: {
-      data,
+      news: data.contents,
     },
   };
 };
-
-// eslint-disable-next-line import/no-default-export
-export default News;
