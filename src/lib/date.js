a6c490687201ac69ad6e5c90b8aabb88ad05522a
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-12-25 14:13:32 +0900

    ⚰️  remove dead code

--- src/lib/date.js
+++ src/lib/date.js
@@ -3,6 +3,5 @@ import { format, parseISO } from "date-fns";
 // eslint-disable-next-line react/destructuring-assignment
 export const Date = ({ dateString }) => {
   const date = parseISO(dateString);
-  // return <time dateTime={dateString}>{format(date, "yyyy年M月d日 HH時MM分")}</time>;
   return <time dateTime={dateString}>{format(date, "yyy LLL ii k:mm")}</time>;
 };
