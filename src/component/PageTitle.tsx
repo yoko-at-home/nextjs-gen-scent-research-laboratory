1e821c2df9b176a76cf14820177b23b40db86331
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-12-25 10:03:00 +0900

    NextPageの導入

--- src/component/PageTitle.tsx
+++ src/component/PageTitle.tsx
@@ -1,10 +1,12 @@
-export const PageTitle = (props) => {
+import type { NextPage } from "next";
+
+export const PageTitle: NextPage = (props) => {
   return (
     <h1 className="mt-20 mb-5 text-3xl font-extrabold tracking-tight leading-9 sm:text-4xl sm:leading-10 md:mt-24 md:mb-16 md:text-5xl md:leading-10 lg:mb-10">
       {props.children}
     </h1>
   );
 };
-export const PageSubTitle = (props) => {
+export const PageSubTitle: NextPage = (props) => {
   return <h2 className="mt-10 mb-5 text-2xl font-bold md:mt-14 md:mb-8 md:text-3xl">{props.children}</h2>;
 };
