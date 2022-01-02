5bb50bffa09364a9e115a61c9dbfaf8a6a9255a7
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-01-02 14:12:23 +0900

    FixedLayout:mx-5を復活

--- src/layout/FixedLayout.tsx
+++ src/layout/FixedLayout.tsx
@@ -18,7 +18,7 @@ export const FixedLayout: VFC<Props> = (props) => {
   return (
     <div className="grid grid-rows-[auto,1fr,auto] mx-auto max-w-screen-lg min-h-screen">
       <HeaderFixed />
-      <main className="text-zinc-500">
+      <main className="mx-5 text-zinc-500">
         <LayoutErrorBoundary>{props.children}</LayoutErrorBoundary>
       </main>
       <div className="flex justify-between mt-16">
