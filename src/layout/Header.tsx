7744f5252e7d7cd2ce227b316589bf351fefb551
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-01-03 19:33:02 +0900

    Lighthouse: スコア改善対策

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -54,8 +54,8 @@ export const Header: VFC = () => {
       <nav className="flex flex-wrap justify-center text-gray-600 bg-gray-200">
         {items.map(({ href, label }) => {
           return (
-            <NavLink key={href} href={href} activeClassName="bg-gray-500 text-white">
-              <a className="inline-block p-1 font-caribri text-base font-semibold hover:text-gray-200 whitespace-nowrap hover:bg-gray-400 bg-opacity-30 bg-gradient-to-r focus:from-gray-300 focus:to-[#330033] sm:p-4">
+            <NavLink key={href} href={href} activeClassName="bg-gray-700 text-white">
+              <a className="inline-block p-1 font-caribri text-base font-semibold hover:text-gray-200 whitespace-nowrap hover:bg-gray-400 bg-opacity-60 bg-gradient-to-r focus:from-gray-300 focus:to-[#330033] sm:p-4">
                 {label}
               </a>
             </NavLink>
