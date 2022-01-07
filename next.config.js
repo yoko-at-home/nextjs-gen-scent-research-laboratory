7cfc7f8a5f437ee0441e105890da50409479a1d6
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-01-07 17:32:56 +0900

    next-pwaをインポート

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    runtimeCaching,
+  },
+});
 module.exports = {
   i18n: { locales: ["ja"], defaultLocale: "ja" },
   reactStrictMode: true,
