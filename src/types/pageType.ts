7a5aa3b932294312402889ef9af7de3aebd3afb8
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-01-07 22:00:32 +0900

    🚚 コードの移動（page props typeを分割

--- src/types/pageType.ts
+++ src/types/pageType.ts
@@ -1,3 +1,63 @@
+export type HomeProps = {
+  data: {
+    body: HTMLAnchorElement;
+    createdAt: Date;
+    publishedAt: Date;
+    revisedAt: Date;
+    title: string;
+    updatedAt: Date;
+  };
+};
+
+export type ProductProps = {
+  odor: [
+    {
+      description: string;
+      id: string;
+      image: { url: string; height: number; width: number };
+      title: string;
+    }
+  ];
+  other: [
+    {
+      description: string;
+      id: string;
+      image: { url: string; height: number; width: number };
+      note: string;
+      title: string;
+    }
+  ];
+  sample: [
+    {
+      description: string;
+      id: string;
+      image: { url: string; height: number; width: number };
+      title: string;
+      product_title: string;
+    }
+  ];
+  software: [
+    {
+      description: string;
+      id: string;
+      image: { url: string; height: number; width: number };
+      title: string;
+      subtitle: string;
+      product_title: string;
+    }
+  ];
+  software2: [
+    {
+      description: string;
+      id: string;
+      image: { url: string; height: number; width: number };
+      title: string;
+      subtitle: string;
+      product_title: string;
+    }
+  ];
+};
+
 export type NewsProps = {
   news: [
     {
--- src/types/type.ts
+++ src/types/type.ts
@@ -1,89 +1,3 @@
-export type HomeProps = {
-  data: {
-    body: HTMLAnchorElement;
-    createdAt: Date;
-    publishedAt: Date;
-    revisedAt: Date;
-    title: string;
-    updatedAt: Date;
-  };
-};
-
-export type ProductProps = {
-  odor: [
-    {
-      description: string;
-      id: string;
-      image: { url: string; height: number; width: number };
-      title: string;
-    }
-  ];
-  other: [
-    {
-      description: string;
-      id: string;
-      image: { url: string; height: number; width: number };
-      note: string;
-      title: string;
-    }
-  ];
-  sample: [
-    {
-      description: string;
-      id: string;
-      image: { url: string; height: number; width: number };
-      title: string;
-      product_title: string;
-    }
-  ];
-  software: [
-    {
-      description: string;
-      id: string;
-      image: { url: string; height: number; width: number };
-      title: string;
-      subtitle: string;
-      product_title: string;
-    }
-  ];
-  software2: [
-    {
-      description: string;
-      id: string;
-      image: { url: string; height: number; width: number };
-      title: string;
-      subtitle: string;
-      product_title: string;
-    }
-  ];
-};
-
-export type ApplicationProps = {
-  application: [
-    {
-      body: HTMLAnchorElement;
-      createdAt: Date;
-      description: string;
-      id: string;
-      publishedAt: Date;
-      revisedAt: Date;
-      title: string;
-      updatedAt: Date;
-    }
-  ];
-};
-
-export type PrivateProps = {
-  data: {
-    body?: HTMLAnchorElement;
-    createdAt: Date;
-    publishedAt: Date;
-    revisedAt: Date;
-    title: string;
-    updatedAt: Date;
-  };
-};
-
 export type SoftwareProps = {
   software: {
     body: HTMLAnchorElement;
@@ -203,17 +117,6 @@ export type OtherProps = {
     publishedAt: Date;
     revisedAt: Date;
     subtitle: string;
-    title: string;
-    updatedAt: Date;
-  };
-};
-export type NewsProps = {
-  news: {
-    body: HTMLAnchorElement;
-    createdAt: Date;
-    description: string;
-    publishedAt: Date;
-    revisedAt: Date;
     title: string;
     updatedAt: Date;
   };
