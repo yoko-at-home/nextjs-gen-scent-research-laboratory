ed4b84cf287bd0ab2816189a0e73f4e32fc9daa5
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-01-14 22:31:33 +0900

    参考コードをコピー

--- src/pages/api/preview.js
+++ src/pages/api/preview.js
@@ -1,43 +1,28 @@
-import axios from "axios";
+import fetch from "node-fetch";
 
-const preview = async (req, res) => {
-  // クエリの確認
+export default async function preview(req, res) {
   if (!req.query.slug) {
     return res.status(404).end();
   }
-  // if (req.query.secret !== process.env.CMS_API_KEY || !content.id || !req.query.draftKey) {
-  //   return res.status(401).json({ message: `Invalid query, ${process.env.CMS_API_KEY}` });
-  // }
+  const content = await fetch(
+    `https://genscent.microcms.io/api/v1/news/${req.query.slug}?fields=id&draftKey=${req.query.draftKey}`,
+    { headers: { "X-API-KEY": process.env.CMS_API_KEY || "" } }
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .catch((error) => {
+      return console.error(error);
+    });
 
-  // 下書きのデータを取得
-  const key = {
-    headers: { "X-MICROCMS-API-KEY": process.env.CMS_API_KEY || "" },
-  };
-  // console.log(content.id);
-
-  const content =
-    `${process.env.NEXT_PUBLIC_API_URL}news/` + `${req.query.slug}?fields=id&draftKey=${req.query.draftKey}`;
-  const url = `${process.env.NEXT_PUBLIC_API_URL}news/` + content.id + `?draftKey=${req.query.draftKey}`;
-  const post = await axios.get(url, key);
-
-  // エラー処理
-  if (!post) {
-    return res.status(401).json({ message: "Invalid draft key" });
-  }
   if (!content) {
     return res.status(401).json({ message: "Invalid slug" });
   }
 
-  // プレビューデータを格納
   res.setPreviewData({
-    draftKey: req.query.draftKey,
     slug: content.id,
+    draftKey: req.query.draftKey,
   });
-
-  // 詳細ページへリダイレクト
   res.writeHead(307, { Location: `/news/${content.id}` });
-
   res.end("Preview mode enabled");
-};
-
-export default preview;
+}
