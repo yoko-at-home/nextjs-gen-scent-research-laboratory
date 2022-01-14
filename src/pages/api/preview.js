2ac3c24de84a92ba8669b83e7ee7fbfc7100bc42
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-01-14 22:20:26 +0900

    req.query.idをcontent.idに変更

--- src/pages/api/preview.js
+++ src/pages/api/preview.js
@@ -1,12 +1,11 @@
 import axios from "axios";
-import type { NextApiHandler } from "next";
 
-const preview: NextApiHandler = async (req, res) => {
+const preview = async (req, res) => {
   // クエリの確認
   if (!req.query.slug) {
     return res.status(404).end();
   }
-  // if (req.query.secret !== process.env.CMS_API_KEY || !req.query.id || !req.query.draftKey) {
+  // if (req.query.secret !== process.env.CMS_API_KEY || !content.id || !req.query.draftKey) {
   //   return res.status(401).json({ message: `Invalid query, ${process.env.CMS_API_KEY}` });
   // }
 
@@ -14,11 +13,11 @@ const preview: NextApiHandler = async (req, res) => {
   const key = {
     headers: { "X-MICROCMS-API-KEY": process.env.CMS_API_KEY || "" },
   };
-  // console.log(req.query.id);
+  // console.log(content.id);
 
   const content =
     `${process.env.NEXT_PUBLIC_API_URL}news/` + `${req.query.slug}?fields=id&draftKey=${req.query.draftKey}`;
-  const url = `${process.env.NEXT_PUBLIC_API_URL}news/` + req.query.id + `?draftKey=${req.query.draftKey}`;
+  const url = `${process.env.NEXT_PUBLIC_API_URL}news/` + content.id + `?draftKey=${req.query.draftKey}`;
   const post = await axios.get(url, key);
 
   // エラー処理
@@ -32,11 +31,11 @@ const preview: NextApiHandler = async (req, res) => {
   // プレビューデータを格納
   res.setPreviewData({
     draftKey: req.query.draftKey,
-    slug: req.query.id,
+    slug: content.id,
   });
 
   // 詳細ページへリダイレクト
-  res.writeHead(307, { Location: `/news/${req.query.id}` });
+  res.writeHead(307, { Location: `/news/${content.id}` });
 
   res.end("Preview mode enabled");
 };
