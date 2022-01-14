2f4052dcd2833d6173eb23edbd2bfc25705f8eef
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-01-14 20:59:53 +0900

    404のコードを挿入

--- src/pages/api/preview.ts
+++ src/pages/api/preview.ts
@@ -3,6 +3,9 @@ import type { NextApiHandler } from "next";
 
 const preview: NextApiHandler = async (req, res) => {
   // クエリの確認
+  if (!req.query.slug) {
+    return res.status(404).end();
+  }
   if (req.query.secret !== process.env.CMS_API_KEY || !req.query.id || !req.query.draftKey) {
     return res.status(401).json({ message: `Invalid query, ${process.env.CMS_API_KEY}` });
   }
