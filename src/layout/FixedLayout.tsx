be4f4b8c339122d834ee08ad24c72d7213cebe37
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-01-15 22:27:56 +0900

    FixedLayout: main mtを設定

--- src/layout/FixedLayout.tsx
+++ src/layout/FixedLayout.tsx
@@ -17,7 +17,7 @@ export const FixedLayout: VFC<Props> = (props) => {
   return (
     <div className="grid grid-rows-[auto,1fr,auto] mx-auto min-h-screen">
       <Header opacity="80" />
-      <main className="mx-5 text-zinc-500 sm:mx-20 lg:mx-32">
+      <main className="mx-5 text-zinc-500 sm:mx-20 md:mt-16 lg:mx-32 mt8">
         <LayoutErrorBoundary>{props.children}</LayoutErrorBoundary>
       </main>
       <div className="flex justify-around items-center mt-20">
