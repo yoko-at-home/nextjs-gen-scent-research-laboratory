3d77e8ff20a815018ca20a0fc771333a878fa0da
yoko-web <tw107sf.ham@icloud.com>
2022-01-18 18:08:01 +0900

    Preview: news [id].js に戻す

---
+++ src/pages/news/[id].js
@@ -0,0 +1,77 @@
+/* eslint-disable @typescript-eslint/naming-convention*/
+import ErrorPage from "next/error";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { PageSubTitle } from "src/component/PageTitle";
+import { PageSEO } from "src/component/SEO";
+import { siteMetadata } from "src/data/siteMetaData";
+import { FixedLayout } from "src/layout";
+
+const NewsId = (props) => {
+  // if (!props.data) {
+  //   return <ErrorPage statusCode={404} />;
+  // }
+  const router = useRouter();
+
+  if (router.isFallback && !news?.id) {
+    return <ErrorPage statusCode={404} />;
+  }
+  return (
+    <FixedLayout>
+      <PageSEO
+        title={`${props.data.title} - News - ${siteMetadata.title}`}
+        description={siteMetadata.description}
+        ogType="website"
+        ogImage={siteMetadata.siteUrl + siteMetadata.siteLogo}
+        siteUrl={siteMetadata.siteUrl}
+      />
+      <main className="break-all">
+        {props.preview && <Link href="/api/clear-preview">プレビューモードを解除</Link>}
+        <PageSubTitle fontWeight="ordinary">{props.data.title}</PageSubTitle>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `${props.data.body}`,
+          }}
+        />
+      </main>
+    </FixedLayout>
+  );
+};
+
+export default NewsId;
+
+export const getStaticPaths = async () => {
+  const key = {
+    headers: { "X-MICROCMS-API-KEY": process.env.CMS_API_KEY },
+  };
+
+  const res = await fetch("https://genscent.microcms.io/api/v1/news", key);
+  const repos = await res.json();
+
+  const paths = repos.contents.map((repo) => {
+    return `/news/${repo.id}`;
+  });
+  return { paths, fallback: true };
+};
+
+export const getStaticProps = async ({ params, preview = false, previewData }) => {
+  const id = params.id;
+  const draftKey = previewData?.draftKey;
+  const key = {
+    headers: { "X-MICROCMS-API-KEY": process.env.CMS_API_KEY },
+  };
+
+  const res = await fetch(
+    `https://genscent.microcms.io/api/v1/news/${id}?${draftKey !== undefined ? `draftKey=${draftKey}` : ""}`,
+    key
+  );
+  const data = await res.json();
+
+  return {
+    props: {
+      data: data,
+      preview,
+    },
+    revalidate: 1,
+  };
+};
