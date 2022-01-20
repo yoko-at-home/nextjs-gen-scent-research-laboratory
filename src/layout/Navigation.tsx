3edf8c2d7ecb329b414728392f64770650928d37
yoko-web <tw107sf.ham@icloud.com>
2022-01-20 21:33:27 +0900

    lighthouse: mobile時のpadding調整

--- src/layout/Navigation.tsx
+++ src/layout/Navigation.tsx
@@ -36,7 +36,7 @@ const Navigation = () => {
       {items.map(({ href, label }) => {
         return (
           <NavLink key={href} href={href} activeClassName="bg-[#291a29] text-white bg-opacity-40">
-            <a className="inline-block p-1 font-caribri text-base font-semibold hover:text-gray-200 whitespace-nowrap hover:bg-gray-400 bg-gradient-to-r focus:from-gray-300 focus:to-[#330033] sm:p-4 bg-opacity-80">
+            <a className="inline-block py-1 px-3 font-caribri text-base font-semibold hover:text-gray-200 whitespace-nowrap hover:bg-gray-400 bg-gradient-to-r focus:from-gray-300 focus:to-[#330033] sm:p-4 bg-opacity-80">
               {label}
             </a>
           </NavLink>
