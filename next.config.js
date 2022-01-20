f1e07aaa20a7b0cc8ff2f4361202a4f93eea1e96
yoko-web <tw107sf.ham@icloud.com>
2022-01-20 11:31:12 +0900

    pwa設定ロジックを追加

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
-module.exports = {
-  i18n: { locales: ["ja"], defaultLocale: "ja" },
+/** @type {import('next').NextConfig} */
+
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
   reactStrictMode: true,
+  i18n: { locales: ["ja"], defaultLocale: "ja" },
   typescript: { ignoreDevErrors: true },
   poweredByHeader: false,
   images: {
     domains: ["images.microcms-assets.io"],
   },
-};
+  pwa: {
+    dest: "public",
+    register: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+});
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,6 @@ const MyDocument = () => {
   return (
     <Html lang="ja-JP" dir="ltr">
       <Head>
-        <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
         <link rel="manifest" href="/static/favicons/site.webmanifest" />
