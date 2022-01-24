e2fbe6a8e59d3b570b519958cbb93b202da45d1c
yoko-web <tw107sf.ham@icloud.com>
2022-01-24 16:05:25 +0900

    tailwind eslint設定を追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -60,6 +60,13 @@ module.exports = {
       "error",
       { components: ["Link"], specialLink: ["hrefLeft", "hrefRight"], aspects: ["invalidHref", "preferButton"] },
     ],
+    // tailwind
+    "tailwindcss/classnames-order": "warn",
+    "tailwindcss/enforces-shorthand": "warn",
+    "tailwindcss/migration-from-tailwind-2": "warn",
+    "tailwindcss/no-arbitrary-value": "off",
+    "tailwindcss/no-custom-classname": "warn",
+    "tailwindcss/no-contradicting-classname": "error",
   },
   overrides: [
     {
