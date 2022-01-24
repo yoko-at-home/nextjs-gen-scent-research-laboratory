612e5d52bcd6b5c135a523e84e4d27ee05757d62
yoko-web <tw107sf.ham@icloud.com>
2022-01-24 13:54:27 +0900

    所属団体を追加

--- src/pages/aboutus/index.tsx
+++ src/pages/aboutus/index.tsx
@@ -3,58 +3,28 @@ import Image from "next/image";
 import { PageTitle } from "src/component/PageTitle";
 import { PageSEO } from "src/component/SEO";
 import { FluidLayout } from "src/layout";
+import { client } from "src/lib/client";
 
 import { siteMetadata } from "../../data/siteMetaData";
 
-const AboutContent = () => {
-  return (
-    <FluidLayout width="main">
-      <PageSEO
-        title={`About Us - ${siteMetadata.title}`}
-        description={siteMetadata.description}
-        ogType="website"
-        ogImage={siteMetadata.siteUrl + siteMetadata.siteLogo}
-        siteUrl={siteMetadata.siteUrl}
-      />
-      <div className="flex flex-col justify-between">
-        <PageTitle>
-          <span className="font-yui text-3xl tracking-widest sm:text-5xl">玄川リサーチ</span>
-        </PageTitle>
-        <h2 className="my-5 font-caribri font-bold sm:text-xl md:text-2xl lg:mb-10 lg:text-3xl animation">
-          Gen-Scent Research Laboratory
-        </h2>
-        <span className="font-caribri font-bold sm:text-lg animation">Shimomeguro, Meguro, Tokyo, Japan</span>
-        <div className="container flex pt-6 pb-8 space-y-2 md:space-y-10 animation">
-          <span className="mr-5 text-xl font-semibold whitespace-nowrap">事業内容</span>
-          <div className="">
-            <p>GC-MS 用ソフトウェア、データベース販売</p>
-            <p>香り分析関連製品販売</p>
-          </div>
-        </div>
-        <div className="container flex-1">
-          <div className="container flex pt-40 pb-3 space-y-2 md:space-y-10 animation">
-            <span className="font-semibold whitespace-nowrap">ロゴマークについて</span>
-          </div>
-          <div className="flex items-center">
-            <div className="mx-6 animation">
-              <Image src={"/static/images/logo.png"} alt="logo" width="80px" height="80px" />
-            </div>
-            <div className="text-sm font-thin animation">
-              玄川の家紋をデザイン化したものです。
-              <br />
-              ”日本の研究者にとって役に立つ製品や技術を提供していきたい”と願う 玄川ファミリーの思いが込められています。
-            </div>
-          </div>
-        </div>
-        <div className="container mt-3 text-xs font-extralight text-left text-gray-300 animation">
-          <sub>写真は明治38年に国内に渡来した月桂樹で、日比谷公園、青山霊園などに植樹されたうちの一株です。</sub>
-        </div>
-      </div>
-    </FluidLayout>
-  );
+type Props = {
+  data: {
+    address: string;
+    description1: string;
+    description2: string;
+    affiliate1: string;
+    affiliate1_url: string;
+    affiliate2: string;
+    affiliate2_url: string;
+    affiliate3: string;
+    affiliate3_url: string;
+    affiliate4: string;
+    affiliate4_url: string;
+    title: string;
+  };
 };
 
-const AboutUs: NextPage = () => {
+const AboutUs: NextPage<Props> = (props) => {
   return (
     <>
       <div
@@ -63,7 +33,75 @@ const AboutUs: NextPage = () => {
           backgroundImage: "url('/static/images/header/background.webp')",
         }}
       >
-        <AboutContent />
+        <FluidLayout width="main">
+          <PageSEO
+            title={`About Us - ${siteMetadata.title}`}
+            description={siteMetadata.description}
+            ogType="website"
+            ogImage={siteMetadata.siteUrl + siteMetadata.siteLogo}
+            siteUrl={siteMetadata.siteUrl}
+          />
+          <div className="flex flex-col justify-between">
+            <PageTitle>
+              <span className="font-yui text-3xl tracking-widest sm:text-5xl">玄川リサーチ</span>
+            </PageTitle>
+            <h2 className="my-5 font-caribri font-bold sm:text-xl md:text-2xl lg:mb-10 lg:text-3xl animation">
+              Gen-Scent Research Laboratory
+            </h2>
+            <span className="font-caribri font-bold sm:text-lg animation">{props.data.address}</span>
+            <div className="container flex pt-6 pb-8 space-y-2 md:space-y-10 animation">
+              <span className="mr-5 text-xl font-semibold whitespace-nowrap">事業内容</span>
+              <div className="">
+                <p>{props.data.description1}</p>
+                <p>{props.data.description2}</p>
+              </div>
+            </div>
+            <div className="container flex pt-6 pb-8 space-y-2 md:space-y-10 animation">
+              <span className="mr-5 text-xl font-semibold whitespace-nowrap">所属団体</span>
+              <div className="">
+                <p>
+                  <a href={props.data.affiliate1_url} target="_blank" rel="noopener noreferrer">
+                    {props.data.affiliate1}
+                  </a>
+                </p>
+                <p>
+                  <a href={props.data.affiliate2_url} target="_blank" rel="noopener noreferrer">
+                    {props.data.affiliate2}
+                  </a>
+                </p>
+                <p>
+                  <a href={props.data.affiliate3_url} target="_blank" rel="noopener noreferrer">
+                    {props.data.affiliate3}
+                  </a>
+                </p>
+                <p>
+                  <a href={props.data.affiliate4_url} target="_blank" rel="noopener noreferrer">
+                    {props.data.affiliate4}
+                  </a>
+                </p>
+              </div>
+            </div>
+            <div className="container flex-1">
+              <div className="container flex pt-40 pb-3 space-y-2 md:space-y-10 animation">
+                <span className="font-semibold whitespace-nowrap">ロゴマークについて</span>
+              </div>
+              <div className="flex items-center">
+                <div className="mx-6 animation">
+                  <Image src={"/static/images/logo.png"} alt="logo" width="80px" height="80px" />
+                </div>
+                <div className="text-sm font-thin animation">
+                  玄川の家紋をデザイン化したものです。
+                  <br />
+                  ”日本の研究者にとって役に立つ製品や技術を提供していきたい”と願う
+                  玄川ファミリーの思いが込められています。
+                </div>
+              </div>
+            </div>
+            <div className="container mt-3 text-xs font-extralight text-left text-gray-300 animation">
+              <sub>写真は明治38年に国内に渡来した月桂樹で、日比谷公園、青山霊園などに植樹されたうちの一株です。</sub>
+            </div>
+          </div>
+        </FluidLayout>
       </div>
       <div
         className="absolute top-0 w-full min-h-screen text-white bg-top bg-cover sm:hidden"
@@ -71,10 +109,89 @@ const AboutUs: NextPage = () => {
           backgroundImage: "url('/static/images/header/background-mobile.webp')",
         }}
       >
-        <AboutContent />
+        <FluidLayout width="main">
+          <PageSEO
+            title={`About Us - ${siteMetadata.title}`}
+            description={siteMetadata.description}
+            ogType="website"
+            ogImage={siteMetadata.siteUrl + siteMetadata.siteLogo}
+            siteUrl={siteMetadata.siteUrl}
+          />
+          <div className="flex flex-col justify-between">
+            <PageTitle>
+              <span className="font-yui text-3xl tracking-widest sm:text-5xl">玄川リサーチ</span>
+            </PageTitle>
+            <h2 className="my-5 font-caribri font-bold sm:text-xl md:text-2xl lg:mb-10 lg:text-3xl animation">
+              Gen-Scent Research Laboratory
+            </h2>
+            <span className="font-caribri font-bold sm:text-lg animation">{props.data.address}</span>
+            <div className="container flex pt-6 pb-8 space-y-2 md:space-y-10 animation">
+              <span className="mr-5 text-xl font-semibold whitespace-nowrap">事業内容</span>
+              <div className="">
+                <p>{props.data.description1}</p>
+                <p>{props.data.description2}</p>
+              </div>
+            </div>
+            <div className="container flex pt-6 pb-8 space-y-2 md:space-y-10 animation">
+              <span className="mr-5 text-xl font-semibold whitespace-nowrap">所属団体</span>
+              <div className="">
+                <p>
+                  <a href={props.data.affiliate1_url} target="_blank" rel="noopener noreferrer">
+                    {props.data.affiliate1}
+                  </a>
+                </p>
+                <p>
+                  <a href={props.data.affiliate2_url} target="_blank" rel="noopener noreferrer">
+                    {props.data.affiliate2}
+                  </a>
+                </p>
+                <p>
+                  <a href={props.data.affiliate3_url} target="_blank" rel="noopener noreferrer">
+                    {props.data.affiliate3}
+                  </a>
+                </p>
+                <p>
+                  <a href={props.data.affiliate4_url} target="_blank" rel="noopener noreferrer">
+                    {props.data.affiliate4}
+                  </a>
+                </p>
+              </div>
+            </div>
+            <div className="container flex-1">
+              <div className="container flex pt-40 pb-3 space-y-2 md:space-y-10 animation">
+                <span className="font-semibold whitespace-nowrap">ロゴマークについて</span>
+              </div>
+              <div className="flex items-center">
+                <div className="mx-6 animation">
+                  <Image src={"/static/images/logo.png"} alt="logo" width="80px" height="80px" />
+                </div>
+                <div className="text-sm font-thin animation">
+                  玄川の家紋をデザイン化したものです。
+                  <br />
+                  ”日本の研究者にとって役に立つ製品や技術を提供していきたい”と願う
+                  玄川ファミリーの思いが込められています。
+                </div>
+              </div>
+            </div>
+            <div className="container mt-3 text-xs font-extralight text-left text-gray-300 animation">
+              <sub>写真は明治38年に国内に渡来した月桂樹で、日比谷公園、青山霊園などに植樹されたうちの一株です。</sub>
+            </div>
+          </div>
+        </FluidLayout>
       </div>
     </>
   );
 };
 
+export const getStaticProps = async () => {
+  const data = await client.get({
+    endpoint: "aboutus",
+  });
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
 export default AboutUs;
