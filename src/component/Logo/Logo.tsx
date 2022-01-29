a590e3f5ecaaf5585c67754c310dd6af83c8c93d
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-01-29 15:22:51 +0900

    Lighthouse: introduce SVG

--- src/component/Logo/Logo.tsx
+++ src/component/Logo/Logo.tsx
@@ -2,26 +2,22 @@ import Image from "next/image";
 import Link from "next/link";
 
 export const Logo = () => {
-  const src = "/static/images/logo.png";
-
   return (
     <div className="hidden sm:block">
       <Link href="/" aria-label="Gen-Scent Research Laboratoryロゴ" passHref>
-        {/* <a aria-label="ホームへ"> */}
-        <Image src={src} alt="Gen-Scent Research Laboratoryロゴ" width="60px" height="60px" />
-        {/* </a> */}
+        <a aria-label="ホームへ">
+          <Image src="/static/Logo.svg" alt="Gen-Scent Research Laboratoryロゴ" width={60} height={60} />
+        </a>
       </Link>
     </div>
   );
 };
 export const LogoSmall = () => {
-  const src = "/static/images/logo.png";
-
   return (
     <div className="sm:hidden">
       <Link href="/" aria-label="Gen-Scent Research Laboratoryロゴ" passHref>
         {/* <a aria-label="ホームへ"> */}
-        <Image src={src} alt="Gen-Scent Research Laboratoryロゴ" width="40px" height="40px" />
+        <Image src="/static/Logo.svg" alt="Gen-Scent Research Laboratoryロゴ" width={40} height={40} />
         {/* </a> */}
       </Link>
     </div>
