7cbba2d5b279fc7f5097bc5991f12192b6addf02
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-01-31 14:08:39 +0900

    fix

--- src/layout/FixedLayout.tsx
+++ src/layout/FixedLayout.tsx
@@ -17,7 +17,7 @@ export const FixedLayout: VFC<Props> = (props) => {
   return (
     <div className="grid grid-rows-[auto,1fr,auto] mx-auto min-h-screen">
       <Header opacity="80" />
-      <main className="mx-5 text-zinc-500 sm:mx-20 md:mt-16 lg:mx-32 mt8">
+      <main className="mx-5 mt-8 text-zinc-500 sm:mx-20 md:mt-16 lg:mx-32">
         <LayoutErrorBoundary>{props.children}</LayoutErrorBoundary>
       </main>
       <div className="flex justify-around items-center mt-20 h-24">
