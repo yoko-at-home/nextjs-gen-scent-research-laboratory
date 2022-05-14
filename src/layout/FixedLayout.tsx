976279b5a9b569fec1e1e8f3a84536fc79ceb90d
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-05-14 12:05:19 +0900

    intersection observerでCSSを変更

--- src/layout/FixedLayout.tsx
+++ src/layout/FixedLayout.tsx
@@ -1,5 +1,7 @@
 import type { FC, ReactNode } from "react";
+import { useInView } from "react-intersection-observer";
 import { Button } from "src/component/Button";
+import Navigation from "src/layout/Navigation";
 
 import { Footer } from "./Footer";
 import { Header } from "./Header";
@@ -8,15 +10,21 @@ import { LayoutErrorBoundary } from "./LayoutErrorBoundary";
 type Props = {
   className?: string;
   children: ReactNode;
+  inView?: boolean;
 };
 
 /**
  * @package
  */
 export const FixedLayout: FC<Props> = (props) => {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  const { inView, ref } = useInView({ threshold: 0 });
+
   return (
-    <div className="grid grid-rows-[auto,1fr,auto] mx-auto min-h-screen">
+    <div className="relative mx-auto min-h-screen">
       <Header opacity="80" />
+      <Navigation inView={inView} />
+      <div ref={ref} />
       <main className="mx-5 mt-8 text-zinc-500 sm:mx-20 md:mt-16 lg:mx-32">
         <LayoutErrorBoundary>{props.children}</LayoutErrorBoundary>
       </main>
--- src/layout/FluidLayout.tsx
+++ src/layout/FluidLayout.tsx
@@ -1,6 +1,8 @@
 import cc from "classcat";
 import { useRouter } from "next/router";
 import type { FC, ReactNode } from "react";
+import { useInView } from "react-intersection-observer";
+import Navigation from "src/layout/Navigation";
 
 import { Footer } from "./Footer";
 import { Header } from "./Header";
@@ -10,17 +12,22 @@ type Props = {
   className?: string;
   width?: "main" | "product";
   children: ReactNode;
+  inView?: boolean;
 };
 
 /**
  * @package
  */
 export const FluidLayout: FC<Props> = (props) => {
+  /* eslint-disable @typescript-eslint/naming-convention */
+  const { inView, ref } = useInView({ threshold: 0 });
   const router = useRouter();
   const url = "/aboutus";
   return (
-    <div className="grid grid-rows-[auto,1fr,auto] min-h-screen">
+    <div className="relative min-h-screen">
       {router.pathname === url ? <Header opacity="0" /> : <Header opacity="100" />}
+      <Navigation inView={inView} />
+      <div ref={ref} />
       <main
         className={cc([
           { "mx-5 sm:mx-10 md:mx-auto md:w-11/12 lg:w-9/12": props.width === "main" },
