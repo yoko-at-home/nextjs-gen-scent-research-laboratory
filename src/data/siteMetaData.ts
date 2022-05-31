055193ec3691ef01c5483f4f1457dcfc9d51f036
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-05-31 20:33:33 +0900

    meta keywordsを設定

--- src/data/siteMetaData.ts
+++ src/data/siteMetaData.ts
@@ -3,6 +3,7 @@ export const siteMetadata = {
   title: "玄川リサーチ Gen-Scent Research Laboratory",
   description:
     "玄川リサーチは、香気成分分析用のソフトウェアや、においの研究に関する製品をご紹介しています。ガスクロマトグラフィ- 質量分析計、その他に関する各種ご相談をお受けいたします。",
+  keywords: "Flavor, Fragrance and Odor Simulator",
   language: "jp-ja",
   siteUrl: "https://www.gen-scent.com",
   siteLogo: "/static/images/logo.png",
